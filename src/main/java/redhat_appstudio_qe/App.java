e2a8297f165e480d7c9524ab8af48f5c605d0679
Pavel Sturc <psturc@redhat.com>
2023-04-05 17:05:52 +0200

    fix: init

---
+++ src/main/java/redhat_appstudio_qe/App.java
@@ -0,0 +1,13 @@
+package redhat_appstudio_qe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
